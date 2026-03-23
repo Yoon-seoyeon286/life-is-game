4faea7d2aeaee2a1a41073355d71d123f94f9388
Hydra <panopticon0847@gmail.com>
2026-03-23 10:54:42 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'life-is-game-v1';
+const BASE = '/life-is-game';
+
+const PRECACHE = [
+  BASE + '/',
+  BASE + '/index.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match(BASE + '/index.html'));
+    })
+  );
+});
